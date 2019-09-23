6b61340ca06c1033b9025695742990ab5ab44194
刘勇 <yongliu19@creditease.cn>
2019-09-23 16:30:04 +0800

    变量命名

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -6,9 +6,9 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [GeneratedPluginRegistrant registerWithRegistry:self];
     
-    FlutterViewController* constroller = (FlutterViewController*) self.window.rootViewController;
-    FlutterMethodChannel* paltformChannel = [FlutterMethodChannel methodChannelWithName:@"com.flutter.simple/platform" binaryMessenger:constroller];
-    [paltformChannel setMethodCallHandler:^(FlutterMethodCall * _Nonnull call, FlutterResult  _Nonnull result) {
+    FlutterViewController* controller = (FlutterViewController*) self.window.rootViewController;
+    FlutterMethodChannel* platformChannel = [FlutterMethodChannel methodChannelWithName:@"com.flutter.simple/platform" binaryMessenger:controller];
+    [platformChannel setMethodCallHandler:^(FlutterMethodCall * _Nonnull call, FlutterResult  _Nonnull result) {
         if([call.method isEqualToString:@"getPlatform"]) {
             result(@"iPhone's Title");
         }
